8bb9b2e7ac8797d1ee107faf2914ac9328e63bbb
Vivien-T <44870403+Vivien-T@users.noreply.github.com>
2020-04-19 18:06:56 +0200

    First import from local

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private Movement snakeMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        snakeMovement = GameObject.Find("Snake_Head").GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            snakeMovement.ModifOrientation(false);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            snakeMovement.ModifOrientation(true);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    
+
+    public Vector2 orientation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        orientation = new Vector2(0, 0.01F);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(orientation);
+        
+    }
+
+    public void ModifOrientation(bool inputRight)
+    {
+        if (inputRight)
+        {
+            orientation = RotateVector(orientation, -1);
+        }
+        else
+        {
+            orientation = RotateVector(orientation, 1);
+        }
+    }
+
+    public Vector2 RotateVector(Vector2 v, float angle)
+    {
+        float radian = angle * Mathf.Deg2Rad;
+        float _x = v.x * Mathf.Cos(radian) - v.y * Mathf.Sin(radian);
+        float _y = v.x * Mathf.Sin(radian) + v.y * Mathf.Cos(radian);
+        return new Vector2(_x, _y);
+    }
+
+    void OnTriggerEnter()
+    {
+        if (this.gameObject.GetComponent<BoxCollider2D>().isTrigger)
+        {
+            print("you lose");
+        }
+    }
+}
